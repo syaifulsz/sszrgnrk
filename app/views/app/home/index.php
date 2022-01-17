733b3703c15c4cebcdf78b76a819777bf39ef3c9
Syaiful Shah Zinan <syaifulshahzinan@gmail.com>
2022-01-18 02:20:33 +0800

    Update list

--- app/views/app/home/index.php
+++ app/views/app/home/index.php
@@ -18,14 +18,21 @@ use app\components\ViewComponent;
     <ul>
         <li>Config</li>
         <li>Router</li>
+        <li>Router Cli</li>
+        <li>Route Item</li>
         <li>Str</li>
         <li>Database</li>
         <li>View</li>
         <li>Request</li>
+        <li>Request Cli</li>
         <li>Cache
             <ul>
                 <li>Memcached</li>
             </ul>
         </li>
     </ul>
+</div>
+<div class="mb-3">
+    <div class=""><strong>rgnrk CLI</strong></div>
+    <code>php rgnrk app:setup</code>
 </div>
